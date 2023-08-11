cb6d8d43ba800a33a4126f54ef58cc0a822cf80c
泓賢 吳 <ztex11020676522@gmail.com>
2023-08-11 15:14:31 +0800

    Add call getScheduleActivityOptions

--- src/app/@pages/schedule-manage/schedule-manage.service.ts
+++ src/app/@pages/schedule-manage/schedule-manage.service.ts
@@ -9,31 +9,31 @@ export class ScheduleManageService {
 
   readonly scheduleFunc = 'schedule-management/';
 
-  constructor(private service: ApiService) {}
+  constructor(private service: ApiService) { }
 
   getScheduleActivitySettingList(): Observable<ResponseModel<Array<ScheduleActivitySetting>>> {
-      return this.service.doGet(this.scheduleFunc);
+    return this.service.doGet(this.scheduleFunc);
   }
 
   getScheduleActivitySettingRow(scheduleId: string): Observable<ResponseModel<ScheduleActivitySetting>> {
-      return this.service.doGet(this.scheduleFunc + scheduleId);
+    return this.service.doGet(this.scheduleFunc + scheduleId);
   }
 
   /** 可選的活動下拉選單 API*/
   getScheduleActivityOptions(): Observable<ResponseModel<Array<ActivitySetting>>> {
-      return this.service.doGet(this.scheduleFunc + 'options');
+    return this.service.doGet(this.scheduleFunc + 'options');
   }
 
   createScheduleSetting(data: ScheduleSettingEditReq): Observable<ResponseModel<any>> {
-      return this.service.doPost(this.scheduleFunc, data);
+    return this.service.doPost(this.scheduleFunc, data);
   }
 
   updateScheduleSetting(scheduleId: string, data: ScheduleSettingEditReq): Observable<ResponseModel<any>> {
-      return this.service.doPut(this.scheduleFunc + scheduleId, data);
+    return this.service.doPut(this.scheduleFunc + scheduleId, data);
   }
 
   deleteScheduleSetting(scheduleId: string): Observable<ResponseModel<any>> {
-      return this.service.doDelete(this.scheduleFunc + scheduleId);
+    return this.service.doDelete(this.scheduleFunc + scheduleId);
   }
 
 }
