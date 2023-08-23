b0f7f471bb1976a4874bad75e230f0925892e628
泓賢 吳 <ztex11020676522@gmail.com>
2023-08-23 12:48:22 +0800

    Add fileservice

---
+++ src/app/@api/services/file.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { ResponseModel } from '@api/models/base.model';
+import { Observable } from 'rxjs';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileService {
+
+  readonly fileFunc = 'file';
+  readonly fileUploadFunc = `${this.fileFunc}/upload/`;
+  readonly fileDownloadFunc = `${this.fileFunc}/downLoad/`;
+
+  constructor(private service: ApiService) { }
+
+  uploadFileService(data: FormData): Observable<ResponseModel<any>> {
+    return this.service.doPost(this.fileUploadFunc, data);
+  }
+
+  downloadFileService(id: string): Observable<ResponseModel<any>> {
+    return this.service.doGet(this.fileDownloadFunc + id);
+  }
+
+}
