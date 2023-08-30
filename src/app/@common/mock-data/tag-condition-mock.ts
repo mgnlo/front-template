63b7a8026a5e42290d928da43f4316e0d65438c0
泓賢 吳 <ztex11020676522@gmail.com>
2023-08-30 16:14:43 +0800

    偵測條件改拉取一次，並由前端查詢

--- src/app/@common/mock-data/tag-condition-mock.ts
+++ src/app/@common/mock-data/tag-condition-mock.ts
@@ -8,5 +8,17 @@ export const TagConditionMock: Array<TagCondition> = [
   {
     "conditionKey": "apply-amount_1",
     "conditionName": "申購金額_1"
+  },
+  {
+    "conditionKey": "apply-amount_13",
+    "conditionName": "活動"
+  },
+  {
+    "conditionKey": "apply-amount_12",
+    "conditionName": "動物"
+  },
+  {
+    "conditionKey": "apply-amount_11",
+    "conditionName": "金額"
   }
 ]
