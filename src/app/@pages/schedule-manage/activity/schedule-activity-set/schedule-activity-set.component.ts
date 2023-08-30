a2260b053c65f8aaa5a32215526fbc8c6aaad18c
泓賢 吳 <ztex11020676522@gmail.com>
2023-08-30 13:51:17 +0800

    Update

--- src/app/@pages/schedule-manage/activity/schedule-activity-set/schedule-activity-set.component.ts
+++ src/app/@pages/schedule-manage/activity/schedule-activity-set/schedule-activity-set.component.ts
@@ -162,7 +162,6 @@ export class ScheduleAddComponent extends BaseComponent implements OnInit {
 
   //#region 頻率切換
   changeFrequencyType(key: string) {
-    console.log(key)
     this.removeFieldIfExists('daily');
     this.removeFieldIfExists('hour');
     this.removeFieldIfExists('minute');
