4029ca339b9035ca05a8b7dac4cfe988819c110a
megan.luo <Git:https://git.webcomm.com.tw>
2023-09-13 11:18:38 +0800

    mock ssoLogin UserProfile

---
+++ src/app/@common/mock-data/user-profile-mock.ts
@@ -0,0 +1,134 @@
+import { ConsoleUserList } from "@api/models/console-user.model";
+
+export const UserProfileMock: ConsoleUserList = {
+  "userId": "U66972ba05c34190a5bfd576dad10493",
+  "account": "admin",
+  "name": "Admin",
+  "email": "admin@webcomm.com.tw",
+  "businessUnit": "Digital_Financial",
+  "enable": true,
+  "lastLoginTime": "2023-07-01 09:00:00",
+  "consoleGroup": {
+      "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+      "groupName": "系統管理員",
+      "description": "系統管理員",
+      "priority": 2,
+      "enable": true,
+      "createTime": "2023-07-03 15:30:00",
+      "modificationTime": "2023-08-31 13:21:32",
+      "consoleGroupScope": [
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "activity.create"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "activity.delete"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "activity.read"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "activity.update"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "console-group.create"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "console-group.read"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "console-group.review"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "console-group.update"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "console-user.read"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "console-user.update"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "customer.read"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "dashboard.read"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "review-activity.read"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "review-activity.update"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "review-schedule.read"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "review-schedule.update"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "review-tag.read"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "review-tag.update"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "schedule-activity.create"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "schedule-activity.delete"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "schedule-activity.read"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "schedule-activity.update"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "schedule-tag.read"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "schedule-tag.update"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "tag.create"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "tag.delete"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "tag.read"
+          },
+          {
+              "groupId": "c0ba786046ed4cfe8188455ce1d94960",
+              "scope": "tag.update"
+          }
+      ]
+  }
+}
